3b7e51b1e22d9f982cb026909c86732736de4feb
Alex Cera <devalexcera@gmail.com>
2024-02-05 19:31:44 -0500

    (component) Bird and Obstacle componentes para construir el juego.

---
+++ src/app/home/components/bird/bird.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bird',
+  templateUrl: './bird.component.html',
+  styleUrls: ['./bird.component.scss'],
+})
+export class BirdComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
