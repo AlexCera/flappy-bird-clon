4717a44b3ae97f0f4176e5f7539be5a8d2406d49
Alex Cera <devalexcera@gmail.com>
2024-02-05 21:33:27 -0500

    (feat) pantalla principal del juego funcional.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';;
+import { IonicModule, Platform } from "@ionic/angular";
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from './components/components.module';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule, ComponentsModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  container_height !: number; // alto del contenedor
+  container_width !: number; //ancho del contenedor
+
+  gameStarted: boolean = false; // ¿juego iniciado?
+  gameOver: boolean = false; // ¿perdio?
+
+  score: number = 0; // puntuación final
+
+  musicActive: boolean = false;
+  song = new Audio('/assets/music/ionic-bird-track.MP3');
+
+  constructor(
+    private platform: Platform
+  ) { }
+
+  ngOnInit(): void {
+    this.setContainerSize();
+  }
+
+  /* ========== Tamaño del contenedor del juego ========== */
+  setContainerSize() {
+    this.container_height = this.platform.height();
+    this.container_width = this.platform.width() < 576 ? this.platform.width() : 576;
+  }
+
+
+  playMusic() {
+    this.musicActive = !this.musicActive;
+
+    if (this.musicActive) {
+      this.song.play();
+      this.song.loop;
+    }
+    else {
+      this.song.pause();
+    }
+  }
 }
